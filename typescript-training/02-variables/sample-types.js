8c17466ff51153ae4412bb0dc286495f9168fa0b
guehara-code <gil_uehara@yahoo.com.br>
2023-08-10 09:22:18 -0300

    more studies

---
+++ typescript-training/02-variables/sample-types.js
@@ -0,0 +1,11 @@
+var found = true;
+var grade = 88.6;
+var firstsName = "Anup";
+var lastName = "Kumar";
+// let's break it
+//found = 0;
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstsName + " " + lastName);
+// use template string;
+console.log("Hi ".concat(firstsName, " ").concat(lastName));
---
+++ typescript-training/02-variables/sample-types.ts
@@ -0,0 +1,15 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstsName: string = "Anup";
+let lastName: string = "Kumar";
+
+// let's break it
+//found = 0;
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstsName + " " + lastName);
+
+// use template string;
+
+console.log(`Hi ${firstsName} ${lastName}`);
