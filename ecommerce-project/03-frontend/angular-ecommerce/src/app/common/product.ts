7cedc969e32a64e82336b09a3c8e7bd8c2ab8039
guehara-code <gil_uehara@yahoo.com.br>
2023-08-13 06:39:53 -0300

    more studies

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,34 @@
+export class Product {
+
+    public sku: string;
+    public name: string;
+    public description: string;
+    public unitPrice: number;
+    public imageUrl: string;
+    public active: boolean;
+    public unitsInStock: number;
+    public dateCreated: Date;
+    public lastUpdated: Date;
+
+    constructor(sku: string,
+                name: string,
+                descripton: string,
+                unitPrice: number,
+                imageUrl: string,
+                active: boolean,
+                unitsInStock: number,
+                dateCreated: Date,
+                lastUpdated: Date) {
+        this.sku = sku;
+        this.name = name;
+        this.description = descripton;
+        this.unitPrice = unitPrice;
+        this.imageUrl = imageUrl;
+        this.active = active;
+        this.unitsInStock = unitsInStock;
+        this.dateCreated = dateCreated;
+        this.lastUpdated = lastUpdated;
+
+    }
+
+}
