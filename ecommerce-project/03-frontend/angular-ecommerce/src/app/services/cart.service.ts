efbaa7944cfb9cf82cd44d360172ccb71fb45b25
guehara-code <gil_uehara@yahoo.com.br>
2023-08-24 06:39:51 -0300

    more studies

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -34,7 +34,7 @@ export class CartService {
 		}
 
 		// check if we found it
-		alreadyExistisInCart = (existingCartItem != undefined);
+		alreadyExistisInCart = (existingCartItem != null);
 
 		if (alreadyExistisInCart) {
 			// increment the quantity
@@ -45,5 +45,39 @@ export class CartService {
 			this.cartItems.push(theCartItem);
 		}
 
+		// compute cart total price and total quantity
+		this.computeCartTotals();
+
+	
+	}
+	computeCartTotals() {
+		
+		let totalPriceValue: number = 0;
+		let totalQuantityValue: number = 0;
+
+		for(let currentCartItem of this.cartItems) {
+			totalPriceValue += currentCartItem.quantity + currentCartItem.unitPrice;
+			totalQuantityValue += currentCartItem.quantity;
+		}
+
+		// publish the new values ... all subscribers will receive the new data
+		this.totalPrice.next(totalPriceValue);
+		this.totalQuantity.next(totalQuantityValue);
+
+		// log cart data just for debugging purpose
+		this.logCartData(totalPriceValue, totalQuantityValue);
+
+		
+
+	}
+
+	logCartData(totalPriceValue: number, totalQuantityValue: number) {
+		console.log(`Contents of the cart`);
+		for(let tempCartItem of this.cartItems) {
+			const subTotalPrice = tempCartItem.quantity*tempCartItem.unitPrice;
+			console.log(`name: ${tempCartItem.name}, quantity= ${tempCartItem.quantity}, unitPrice= ${tempCartItem.unitPrice}`)s;
+		}
+		console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
+		console.log('---------')
 	}
 }
