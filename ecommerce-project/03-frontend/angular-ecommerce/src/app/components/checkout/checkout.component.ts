930d1f4429bee9e818225b9307268d2ce4797fd3
guehara-code <gil_uehara@yahoo.com.br>
2023-08-28 17:03:36 -0300

    more studies

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
 	selector: 'app-checkout',
@@ -13,10 +14,15 @@ export class CheckoutComponent implements OnInit {
 	totalPrice: number = 0;
 	totalQuantity: number = 0;
 
-	private formBuilder: FormBuilder;
+	creditCardYears: number[] = [];
+	creditCardMonths: number[] = [];
 
-	constructor(formBuilder: FormBuilder) { 
+	private formBuilder: FormBuilder;
+	private luv2ShopFormService: Luv2ShopFormService;
+
+	constructor(formBuilder: FormBuilder, luv2ShopService: Luv2ShopFormService) { 
 		this.formBuilder = formBuilder;
+		this.luv2ShopFormService = luv2ShopService;
 	}
 
 	ngOnInit(): void {
@@ -50,6 +56,27 @@ export class CheckoutComponent implements OnInit {
 			}),
 			
 		});
+
+		// populate credit card months
+
+		const startMonth: number = new Date().getMonth() + 1;
+		console.log("startMonth: " + startMonth);
+
+		this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+			data => {
+				console.log("Retrieved credit card months: " + JSON.stringify(data));
+				this.creditCardMonths = data;
+			}
+		);
+
+		// populate credit card years
+
+		this.luv2ShopFormService.getCreditCardYears().subscribe(
+			data => {
+				console.log("Retrieved credit card years: " + JSON.stringify(data));
+				this.creditCardYears = data;
+			}
+		);
 	}
 
 	
