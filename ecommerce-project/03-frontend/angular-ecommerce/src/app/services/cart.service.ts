164327a8b68ff15fa8abd001ed20a7dbf5afe116
guehara-code <gil_uehara@yahoo.com.br>
2023-09-09 15:02:14 -0300

    more studies

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -12,8 +12,20 @@ export class CartService {
 	totalPrice: Subject<number> = new BehaviorSubject<number>(0);
 	totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-	constructor() {
+	//storage: Storage = sessionStorage;
+	storage: Storage = localStorage;
 
+
+	constructor() {
+		// read data from storage
+		let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+		if (data != null) {
+			this.cartItems = data;
+
+			// compute totals based on the data that is read from storage
+			this.computeCartTotals();
+		}
 	}
 
 	addToCart(theCartItem: CartItem) {
@@ -53,6 +65,7 @@ export class CartService {
 
 	
 	}
+
 	computeCartTotals() {
 		
 		let totalPriceValue: number = 0;
@@ -70,8 +83,13 @@ export class CartService {
 		// log cart data just for debugging purpose
 		this.logCartData(totalPriceValue, totalQuantityValue);
 
-		
+		// persist cart data
+		this.persistCartItems();
 
+	}
+
+	persistCartItems() {
+		this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
 	}
 
 	logCartData(totalPriceValue: number, totalQuantityValue: number) {
