ea37e9ce80808c3fc3c3329792a5dd4236ed76cc
guehara-code <gil_uehara@yahoo.com.br>
2023-09-12 19:54:56 -0300

    more studies

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { OktaAuth } from '@okta/okta-auth-js';
 
 import myAppConfig from './config/my-app-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
 
 const oktaConfig = myAppConfig.oidc;
 
@@ -75,7 +76,8 @@ const routes: Routes = [
 		CheckoutComponent,
 		LoginComponent,
 		LoginStatusComponent,
-		MembersPageComponent
+		MembersPageComponent,
+  OrderHistoryComponent
 	],
 	imports: [
 		RouterModule.forRoot(routes),
--- ecommerce-project/03-frontend/angular-ecommerce/src/app/common/order-history.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/common/order-history.ts
@@ -1,10 +1,24 @@
 export class OrderHistory {
+    public id: string;
+    public orderTrackingNumber: string;
+    public totalPrice: number;
+    public totalQuantity: number;
+    public dateCreated: Date;
 
     constructor(
-        public id: string,
-        public orderTrackingNumber: string,
-        public totalPrice: number,
-        public totalQuantity: number.
-        public dateCreated: Data);
+        id: string,
+        orderTrackingNumber: string,
+        totalPrice: number,
+        totalQuantity: number,
+        dateCreated: Date) {
+
+            this.id = id;
+            this.orderTrackingNumber = orderTrackingNumber;
+            this.totalPrice = totalPrice;
+            this.totalQuantity = totalQuantity;
+            this.dateCreated = dateCreated;
+    }
 
 }
+
+
