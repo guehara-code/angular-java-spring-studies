eb7e801db7f8c6390496415d46d6dbc3f55884aa
guehara-code <gil_uehara@yahoo.com.br>
2023-09-13 08:46:26 -0300

    more studies

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -31,6 +31,8 @@ export class CheckoutComponent implements OnInit {
 	shippingAddressStates: State[] = [];
 	billingAddressStates: State[] = [];
 
+	storage: Storage = sessionStorage;
+
 	private formBuilder: FormBuilder;
 	private luv2ShopFormService: Luv2ShopFormService;
 	private cartService: CartService;
@@ -50,11 +52,14 @@ export class CheckoutComponent implements OnInit {
 
 		this.reviewCartDetails();
 
+		// read the use's email address from browser storage
+		const theEmail = JSON.parse(this.storage.getItem('userEmail')!);
+
 		this.checkoutFormGroup = this.formBuilder.group({
 			customer: this.formBuilder.group({
 				firstName: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
 				lastName: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
-				email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
+				email: new FormControl(theEmail, [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
 			}),
 			shippingAddress: this.formBuilder.group({
 				street: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
