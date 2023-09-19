0bc3c5c5e77dddabe3466a64c5d56f77d481de36
guehara-code <gil_uehara@yahoo.com.br>
2023-09-19 08:54:30 -0300

    more studies

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -263,8 +263,10 @@ export class CheckoutComponent implements OnInit {
 		purchase.orderItems = orderItems;
 
 		// compute payment info
-		this.paymentInfo.amount = this.totalPrice * 100;
+		this.paymentInfo.amount = Math.round(this.totalPrice * 100);
 		this.paymentInfo.currency = "USD";
+
+		console.log(`this.paymentInfo.amount: ${this.paymentInfo.amount}`);
 
 		// if valid form then
 		// - create payment intent
@@ -329,6 +331,7 @@ export class CheckoutComponent implements OnInit {
 		this.cartService.cartItems = [];
 		this.cartService.totalPrice.next(0);
 		this.cartService.totalQuantity.next(0);
+		this.cartService.persistCartItems();
 
 		// reset the form
 		this.checkoutFormGroup.reset();
